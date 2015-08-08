a12387e83e66980a8e3332afa8907ea675f32e19
gbhasha <gbhasha@gmail.com>
2015-08-08 16:52:32 +0530

    [galeel,prabhu] Landing Page UI updated styles and added bg images

--- jsx/index.ios.js
+++ jsx/index.ios.js
@@ -43,30 +43,113 @@ onLoadPress : function() {
 
         return (
             <View style={styles.container}>
-                        <TouchableHighlight underlayColor="#AA9999" onPress={this.onLoadPress}>
-                          <Image style={styles.image} source={{uri: this.state.avatarSource}} ></Image>
-                        </TouchableHighlight>
-            </View>
+                        <Image
+                            resizeMode="cover"
+                            source={require('image!bw')}
+                            style={styles.image}  />
+                            <View style={styles.gradientContainer}>
+                            </View>
+                            <View style={styles.introTextContainer}>
+                              <Text style={styles.introText}>
+                                  Match-Try-Buy
+                                </Text>
+                            </View>
+                            <View style={styles.introSubTextContainer}>
+                              <Text style={styles.introSubText}>
+                                  Just upload your photo to match a sandle that you wanna buy!!
+                                </Text>
+                            </View>
+                            <TouchableHighlight underlayColor="#AA9999" onPress={this.onLoadPress}>
+                              <View style={styles.buttonContainer}>
+                                <Text style={styles.buttonText}>
+                                  Upload Photo
+                                </Text>
+                              </View>
+                            </TouchableHighlight>
+           </View>
+
         );
     }
 });
 
 var styles = StyleSheet.create({
     container: {
-        flex            : 1,
-        alignItems      : 'center',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-end',
         backgroundColor : '#F5FCFF',
-        paddingTop      : 30
+        position        : 'relative',
     },
 
-    touchableHighlight : {
-        marginLeft  : 10,
-        marginRight : 10,
+    button : {
+        position: 'absolute',
+        backgroundColor: '#ff0000',
+        bottom  : 30,
     },
 
     image: {
-      width          : 280,
-      height         : 180
+      position  : 'absolute',
+      top       : 20,
+      left      : 0,
+      right     : 0,
+      bottom    : 0,
+    },
+
+    buttonContainer : {
+        fontSize : 16,
+        color: '#ffffff',
+        backgroundColor: 'rgba(105,205,35,0.9)',
+        paddingTop: 15,
+        paddingBottom: 15,
+        paddingLeft: 25,
+        paddingRight: 25,
+        borderRadius: 5,
+        marginBottom: 30,
+    },
+
+    buttonText: {
+      color: '#ffffff',
+      fontSize: 18
+    },
+
+    introText: {
+      color: '#ffffff',
+      fontWeight: 'bold',
+      backgroundColor: 'rgba(0,0,0, 0)',
+      fontSize: 30,
+      paddingBottom: 10,
+      paddingLeft: 20,
+      paddingRight: 20,
+    },
+
+    introTextContainer: {
+      backgroundColor: 'rgba(0,0,0, 0)',
+      borderBottomColor: '#ffffff',
+      borderBottomWidth: 1,
+      marginBottom: 10,
+
+    },
+
+    introSubTextContainer: {
+      backgroundColor: 'rgba(0,0,0,0)',
+      paddingLeft: 20,
+      paddingRight: 20,
+      marginBottom: 40,
+    },
+
+    introSubText : {
+      textAlign: 'center',
+      fontSize: 14,
+      color: '#dddddd',
+    },
+
+    gradientContainer: {
+      backgroundColor: 'rgba(0,0,0,0.3)',
+      position  : 'absolute',
+      top       : 20,
+      left      : 0,
+      right     : 0,
+      bottom    : 0,
     }
 });
 
